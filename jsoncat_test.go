c6ac85addf12b060fad4f06f3fb103ecbf3d3c3f
Magnus Bergmark <magnus.bergmark@gmail.com>
2014-06-13 09:19:55 +0200

    Add tests for merging JSON arrays

---
+++ jsoncat_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+)
+
+func parseJson(data string) interface{} {
+	var unmarshaled interface{}
+	err := json.Unmarshal([]byte(data), &unmarshaled)
+	check(err)
+	return unmarshaled
+}
+
+func expectEqual(t *testing.T, methodName string, in, expected, actual interface{}) {
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("%s(%+v) = %+v, but should be %+v", methodName, in, actual, expected)
+	}
+}
+
+func TestMergeJson_arrays(t *testing.T) {
+	in := make([]interface{}, 2)
+	in[0] = parseJson("[1, 2, 3]")
+	in[1] = parseJson("[4, 5, 6]")
+
+	expected := parseJson("[1, 2, 3, 4, 5, 6]")
+
+	expectEqual(t, "MergeJson", in, expected, MergeJson(in))
+}
+
+func TestMergeJson_arrays_of_different_types(t *testing.T) {
+	in := make([]interface{}, 3)
+	in[0] = parseJson("[1337]")
+	in[1] = parseJson("[\"hello\"]")
+	in[2] = parseJson("[\"world\"]")
+
+	expected := parseJson("[1337, \"hello\", \"world\"]")
+
+	expectEqual(t, "MergeJson", in, expected, MergeJson(in))
+}
